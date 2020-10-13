d9ec946024ab1963990326c065889fcd412b8892
victor-frances <72807113+victor-frances@users.noreply.github.com>
2020-10-13 16:09:56 +0200

    Update correo.py

--- correo.py
+++ correo.py
@@ -1,3 +1,9 @@
 # Define aquí mismo la función my_email sin argumentos que 
 # devuelve tu correo electrónico de la UCLM
 
+def my_email():
+
+    correo = 'victor.frances@alu.uclm.es'
+
+    return correo
+
